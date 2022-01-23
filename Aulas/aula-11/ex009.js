7bb835b361a9816c4d58bde9898daad569c7d9fa
GuilhermePereiraGuimaraes <91920230+GuilhermePereiraGuimaraes@users.noreply.github.com>
2022-01-23 03:32:54 -0300

    criação da pasta da aula 11 e conclusão de seus exercícios

---
+++ Aulas/aula-11/ex009.js
@@ -0,0 +1,13 @@
+var país = 'França'
+var last = país.charAt(país.length -1)
+if (last=='a') {
+    console.log(`Vivendo na ${país}`)
+} else {
+    console.log(`Vivendo no ${país}`)
+}
+
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
