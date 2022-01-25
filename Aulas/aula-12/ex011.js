fa7468338c0679093eb49466403fd475408b2344
GuilhermePereiraGuimaraes <91920230+GuilhermePereiraGuimaraes@users.noreply.github.com>
2022-01-24 21:41:36 -0300

    Create ex011.js

---
+++ Aulas/aula-12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 19
+if (idade>=16) {
+    console.log('Pode votar')
+    if (idade>=18) {
+        console.log('E é maior de idade')
+    } else {
+        console.log('Porém é menor de idade')
+    }
+} else {
+    console.log('Não pode votar')
+}
