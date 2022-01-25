dc88dc529bcf1a07109bb9610a62e63d997d4734
GuilhermePereiraGuimaraes <91920230+GuilhermePereiraGuimaraes@users.noreply.github.com>
2022-01-24 22:03:07 -0300

    Algumas atualizações

--- Aulas/aula-12/ex011.js
+++ Aulas/aula-12/ex011.js
@@ -1,11 +1,10 @@
-var idade = 19
-if (idade>=16) {
-    console.log('Pode votar')
-    if (idade>=18) {
-        console.log('E é maior de idade')
-    } else {
-        console.log('Porém é menor de idade')
-    }
-} else {
+var idade = 66
+console.log(`Você tem ${idade} anos`)
+if (idade<16) {
     console.log('Não pode votar')
+} else if(idade<18 || idade>65){
+    console.log('Voto opcional')
+} else{
+    console.log('Voto obrigatório')
 }
+
