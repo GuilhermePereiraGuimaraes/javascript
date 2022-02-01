67f09e9903e621b9f3c7d082367dea2e23a23a65
GuilhermePereiraGuimaraes <91920230+GuilhermePereiraGuimaraes@users.noreply.github.com>
2022-01-31 22:32:59 -0300

    Create ambiente.js

---
+++ Aulas/aula-13/ambiente.js
@@ -0,0 +1,9 @@
+var contagem = 1
+/* while (contagem<=20) {
+    console.log(`Contagem em ${contagem}`)
+    contagem++
+} */
+do {
+    console.log(`Contagem em ${contagem}`)
+    contagem++
+} while (contagem<=6);
