f41ed2234e0857099627cde00be5cc0da5354304
GuilhermePereiraGuimaraes <91920230+GuilhermePereiraGuimaraes@users.noreply.github.com>
2022-02-01 21:17:18 -0300

    criação do projeto do exercicio 15

--- Mini-Projetos/contagem/script.js
+++ Mini-Projetos/contagem/script.js
@@ -1,1 +1,24 @@
-alert('Olá!!!!!!!!!')
+var res= document.getElementById('res')
+res.innerHTML='<p>Preparado para contar....</p>'
+
+function contar() {
+    var inicio= document.getElementById('inicio')
+    var fim= document.getElementById('fim')
+    var passos = document.getElementById('passos')
+    var v_inicio = Number(inicio.value)
+    var v_fim = Number(fim.value)
+    var v_passos= Number(passos.value)
+    res.innerText=''
+    
+    if (v_fim>v_inicio && v_passos>0) {
+        var p_res=document.createElement('p')
+        res.appendChild(p_res)
+        for (c = v_inicio; c < v_fim; c+=v_passos) {
+            p_res.innerHTML+=`${c}&#x1F449 `
+        }
+        p_res.innerHTML+='&#x1F3C6'
+    } else {
+        res.innerHTML='<p>Impossível contar</p>'
+    }
+   
+}
